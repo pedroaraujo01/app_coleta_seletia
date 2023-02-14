903ca29ea9eb8ed6686f6ccb3507882ca84ec19b
Eduardo R. Vargas <eduardo.rochavargas@outlook.com>
2023-02-14 09:17:12 -0300

    Add new models of app

--- lib/app/module/models/solicitacoes_model.dart
+++ lib/app/module/models/solicitacoes_model.dart
@@ -1,12 +1,8 @@
-import 'package:app_coleta_seletiva/app/module/models/solicitacoes_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 
-class SolicitacoesColeta {
-
-  final FirebaseFirestore firestore = FirebaseFirestore.instance;
-
+class Solicitacoes extends ChangeNotifier{
   String id;
   String title;
   String description;
@@ -21,8 +17,22 @@ class SolicitacoesColeta {
   int vidro600;
   int vidro800;
 
-
   SolicitacoesColeta({required this.description, required this.title,required this.id, required this.metal, required this.papel, required this.plastico, required this.vidro1000, required this.vidro350, required this.vidro450, required this.vidro500, required this.vidro600, required this.vidro800, required this.status});
+
+
+  // Solicitacoes.fromDocument(DocumentSnapshot document){
+  //   id = document.id;
+  //   status = document['status'] as String;
+  //   metal = document['metal'] as int;
+  //   papel = document['papel'] as int;
+  //   plastico = document['plastico'] as int;
+  //   vidro1000 = document['vidro1000'] as int;
+  //   vidro350 = document['vidro350'] as int;
+  //   vidro450 = document['vidro450'] as int;
+  //   vidro500 = document['vidro500'] as int;
+  //   vidro600 = document['vidro600'] as int;
+  //   vidro800 = document['vidro800'] as int;
+  // }
 
   final FirebaseFirestore firestore = FirebaseFirestore.instance;
   final FirebaseStorage storage = FirebaseStorage.instance;
@@ -41,11 +51,12 @@ class SolicitacoesColeta {
     loading = true;
 
     final Map<String, dynamic> data = {
-      '': [chave],
+      'name': name,
+      'description': description,
     };
 
     if(id == null){
-      final doc = await firestore.collection('solicitacoes').add(data);
+      final doc = await firestore.collection('products').add(data);
       id = doc.id;
     } else {
       await firestoreRef.update(data);
@@ -54,51 +65,9 @@ class SolicitacoesColeta {
 
     loading = false;
   }
-
-  // List<Product> todasSolicitacoes = [];
-
-  // SolicitacoesColeta(){
-  //   carregasSolictacoesColeta();
-  // }
-  //
-  //   return filteredProducts;
-  // }
-  //
-  // Future<void> carregasSolictacoesColeta() async {
-  //   final QuerySnapshot snapSolicitacoes =
-  //   await firestore.collection('products')
-  //       .where('status', isEqualTo: 'criado').get();
-  //
-  //   todasSolicitacoes = snapSolicitacoes.docs.map(
-  //           (d) => Solicitacoes.fromDocument(d)).toList();
-  //
-  //   notifyListeners();
-  // }
-
-  // Future<void> carregasSolictacoesColetasAnalisadas() async {
-  //   final QuerySnapshot snapSolicitacoesAnalisadas =
-  //   await firestore.collection('products')
-  //       .where('status', isEqualTo: 'analisada').get();
-  //
-  //   todasSolicitacoesAnalisadas = snapSolicitacoesAnalisadas.docs.map(
-  //           (d) => Solicitacoes.fromDocument(d)).toList();
-  //
-  //   notifyListeners();
-  // }
-
-
-  static List<SolicitacoesColeta> generateItems(int numberOfItems) {
-    return List<SolicitacoesColeta>.generate(numberOfItems, (int index) {
-      return SolicitacoesColeta(
-        id: index.toString() + '1',
-        title: 'Solicitação #2824${index + 1}',
-        description: 'Descrição da solicitação #2824${index + 1}',
-      );
-    });
-  }
-
   @override
   String toString() {
     return 'Solicitacoes{id: $id, metal: $metal, papel: $papel, plastico: $plastico, vidro1000: $vidro1000,vidro350: $vidro350,vidro450: $vidro450, vidro500: $vidro500,vidro600: $vidro600,vidro800: $vidro800,}';
   }
+  
 }
